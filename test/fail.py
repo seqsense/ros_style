b58503a38b309f626ade856b1dd060be46ff02d5
Atsushi Watanabe <atsushi.w@ieee.org>
2022-07-19 11:20:30 +0900

    Add reusable workflows for linting

---
+++ test/fail.py
@@ -0,0 +1,6 @@
+def function(x: int) -> str:
+    return x
+
+class Foo:
+    def bar():
+        pass
---
+++ test/pass.py
@@ -0,0 +1,7 @@
+def function(x: int) -> str:
+    return "hoge"
+
+
+class Foo:
+    def bar() -> None:
+        pass
